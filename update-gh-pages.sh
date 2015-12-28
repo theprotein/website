01e1424163581181ebf6dd9939655bead8b75583
Ivan Voischev <voischev.ivan@ya.ru>
2015-12-28 17:56:16 +0300

    fix gh-pages script

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -8,18 +8,19 @@ cd gh-pages
 
 mkdir ./index
 
-cp ../public/index/index.html ./index/index.html
-cp ../public/index/index.min.js ./index/index.min.js
-cp ../public/index/index.min.css ./index/index.min.css
+cp ../public/index/index.html ./index.html
+cp ../public/index/index.min.js ./index.min.js
+cp ../public/index/index.min.css ./index.min.css
 
-cp -r ../public/index/freeze ./index
+cp -r ../public/index/freeze ./
 cp -r ../public/i ./
 
-cp ../public/robots.txt index/robots.txt
-cp ../public/favicon.ico index/favicon.ico
-cp ../public/favicon@2x.png index/favicon@2x.png
-cp ../public/p_share_square.png index/p_share_square.png
-cp ../public/p_share_twitter.png index/p_share_twitter.png
+cp ../public/CNAME CNAME
+cp ../public/robots.txt ./robots.txt
+cp ../public/favicon.ico ./favicon.ico
+cp ../public/favicon@2x.png ./favicon@2x.png
+cp ../public/p_share_square.png ./p_share_square.png
+cp ../public/p_share_twitter.png ./p_share_twitter.png
 cp ../public/intro__video.mp4 ./intro__video.mp4
 
 git add -A
